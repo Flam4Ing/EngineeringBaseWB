5a6558298cc1ef739b479bcfc32dce2b9f01484c
an1977drej <an1977drej@gmail.com>
2020-05-17 22:04:50 +0200

    refactoring

--- EB_D_WiringDuct.py
+++ EB_D_WiringDuct.py
@@ -8,15 +8,37 @@ class WiringDuct:
         obj.Proxy = self
 
     def execute(self, fp):
-        V1 = FreeCAD.Vector(0, 0, 0)
-        V2 = FreeCAD.Vector(0, fp.Width, 0)
-        V3 = FreeCAD.Vector(0, fp.Width, fp.Height)
-        V4 = FreeCAD.Vector(0, fp.Width-1, fp.Height)
-        V5 = FreeCAD.Vector(0, fp.Width-1, 1 )
-        V6 = FreeCAD.Vector(0, 1, 1)
-        V7 = FreeCAD.Vector(0, 1, fp.Height)
-        V8 = FreeCAD.Vector(0, 0, fp.Height)
+        self.wdWidth = fp.Width
+        self.wdHeight = fp.Height
+        self.wdLength = fp.Length
 
+
+        import BOPTools.JoinFeatures
+        j = BOPTools.JoinFeatures.makeCutout(name='Cutout')
+        objBase = self.CreateBaseObject()
+        objTool = self.CreateToolObject()
+        j.Base = objBase
+        j.Tool = objTool
+        j.Proxy.execute(j)
+        j.purgeTouched()
+        objBase.ViewObject.hide()
+        objTool.ViewObject.hide()
+        fp.Shape = j.Shape
+        FreeCAD.getDocument(str(FreeCAD.activeDocument().Name)).removeObject(j.Label)
+        FreeCAD.getDocument(str(FreeCAD.activeDocument().Name)).removeObject(objBase.Label)
+        # FreeCAD.getDocument(str(FreeCAD.activeDocument().Name)).removeObject(objTool.Label)
+
+
+    def CreateBaseObject(self):
+        V1 = FreeCAD.Vector(0, 0, 0)
+        V2 = FreeCAD.Vector(0, self.wdWidth, 0)
+        V3 = FreeCAD.Vector(0, self.wdWidth, self.wdHeight)
+        V4 = FreeCAD.Vector(0, self.wdWidth - 1, self.wdHeight)
+        V5 = FreeCAD.Vector(0, self.wdWidth - 1, 1)
+        V6 = FreeCAD.Vector(0, 1, 1)
+        V7 = FreeCAD.Vector(0, 1, self.wdHeight)
+        V8 = FreeCAD.Vector(0, 0, self.wdHeight)
+        #
         L1 = Part.LineSegment(V1, V2)
         L2 = Part.LineSegment(V2, V3)
         L3 = Part.LineSegment(V3, V4)
@@ -25,47 +47,45 @@ class WiringDuct:
         L6 = Part.LineSegment(V6, V7)
         L7 = Part.LineSegment(V7, V8)
         L8 = Part.LineSegment(V8, V1)
-
+        #
         S1 = Part.Shape([L1, L2, L3, L4, L5, L6, L7, L8])
         W = Part.Wire(S1.Edges)
         F = Part.Face(W)
-        railLength = fp.Length
-        P = F.extrude(FreeCAD.Vector(railLength, 0, 0))
-        obj1Part = FreeCAD.ActiveDocument.addObject("Part::Feature", "obj1Part")
-        obj1Part.Shape = P
+        P = F.extrude(FreeCAD.Vector(self.wdLength, 0, 0))
+        objBase = FreeCAD.ActiveDocument.addObject("Part::Feature", "objBase")
+        objBase.Shape = P
+        return objBase
+
+    def CreateToolObject(self):
+        V1 = FreeCAD.Vector(12, 0, 10)
+        V2 = FreeCAD.Vector(12, self.wdWidth-5, 10)
+        V3 = FreeCAD.Vector(15, self.wdWidth, 10)
+        V4 = FreeCAD.Vector(17, self.wdWidth, 10)
+        V5 = FreeCAD.Vector(20, self.wdWidth-5, 10)
+        V6 = FreeCAD.Vector(20, 0, 10)
         #
-        V51 = FreeCAD.Vector(12, 0, 10)
-        V52 = FreeCAD.Vector(12, fp.Width, 10)
-        V53 = FreeCAD.Vector(20, fp.Width, 10)
-        V54 = FreeCAD.Vector(20, 0, 10)
-        L51 = Part.LineSegment(V51, V52)
-        L52 = Part.LineSegment(V52, V53)
-        L53 = Part.LineSegment(V53, V54)
-        L54 = Part.LineSegment(V54, V51)
-        S50 = Part.Shape([L51, L52, L53, L54])
-        W50 = Part.Wire(S50.Edges)
-        F50 = Part.Face(W50)
-        P50 = F50.extrude(FreeCAD.Vector(0, 0, fp.Height))
-        obj2Part = FreeCAD.ActiveDocument.addObject("Part::Feature", "obj2Part")
-        obj2Part.Shape = P50
+        L1 = Part.LineSegment(V1, V2)
+        L2 = Part.LineSegment(V2, V3)
+        L3 = Part.LineSegment(V3, V4)
+        L4 = Part.LineSegment(V4, V5)
+        L5 = Part.LineSegment(V5, V6)
+        L6 = Part.LineSegment(V6, V1)
+        #
+        S = Part.Shape([L1, L2, L3, L4, L5, L6])
+        W = Part.Wire(S.Edges)
+        F = Part.Face(W)
+        P = F.extrude(FreeCAD.Vector(0, 0, self.wdHeight))
+        arrShape = []
+        arrShape.append(P)
+        qtyHoles = int(self.wdLength // 20)
+        for i in range(1, qtyHoles):
+            tc = P.copy()
+            tc.Placement.Base = FreeCAD.Vector(tc.Placement.Base.x + 20 * i, 0, 0)
+            arrShape.append(tc)
 
-        # import  Draft
-        # cut = Draft.cut(obj1Part,obj2Part)
-
-        # fp.Shape = Part.makeCompound([P,P50])
-        # fp.Shape = cut.Shape
-        import BOPTools.JoinFeatures
-        j = BOPTools.JoinFeatures.makeCutout(name='Cutout')
-        j.Base = obj1Part
-        j.Tool = obj2Part
-        j.Proxy.execute(j)
-        j.purgeTouched()
-        obj1Part.ViewObject.hide()
-        obj2Part.ViewObject.hide()
-        fp.Shape = j.Shape
-        FreeCAD.getDocument(str(FreeCAD.activeDocument().Name)).removeObject(j.Label)
-        FreeCAD.getDocument(str(FreeCAD.activeDocument().Name)).removeObject(obj1Part.Label)
-        FreeCAD.getDocument(str(FreeCAD.activeDocument().Name)).removeObject(obj2Part.Label)
+        objTool = FreeCAD.ActiveDocument.addObject("Part::Feature", "objTool")
+        objTool.Shape = Part.makeCompound(arrShape)
+        return objTool
 
 class ViewProviderWiringDuct:
     def __init__(self, obj):
